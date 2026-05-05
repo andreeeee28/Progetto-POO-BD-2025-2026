5b3eac4eddaa27b642a516a68fb87e951fd85e7a
narin <th.smaldone@studenti.unina.it>
2026-05-05 12:50:42 +0200

    Classe Album iniziata

---
+++ src/main/java/model/Album.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Album {
+
+    private String titolo;
+    private Date dataPubblicazione;
+    private float voto;
+    private Artista artista;
+    private ArrayList<Canzone> tracklist;
+    private ArrayList<Genere> generi;
+    private ArrayList<Recensione> recensioni;
+
+    //Costruttore
+    public Album(String titolo, Date dataPubblicazione, Artista artista, Genere genere, int noCanzoni){
+        Scanner kbdInput = new Scanner(System.in);
+
+        this.titolo = titolo;
+        this.dataPubblicazione = dataPubblicazione;
+        this.artista = artista;
+        generi.add(genere);
+
+        String titoloCanzone;
+        int durataCanzone;
+        for(int i = 0; i<noCanzoni; i++){
+            System.out.println("Digitare il titolo della traccia: ");
+            titoloCanzone = kbdInput.nextLine();
+            System.out.println("Digitare la durata della traccia: ");
+            durataCanzone = kbdInput.nextInt();
+            tracklist.add(new Canzone(titoloCanzone, durataCanzone));
+        }
+    }
+
+
+
+}
